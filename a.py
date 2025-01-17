09d8b48731769e33abb110c7cac55c216b70af46
kupvaz <kupvaz@yandex.ru>
2025-01-17 21:07:53 +0300

    ive done todolist

--- a.py
+++ a.py
@@ -9,11 +9,64 @@ def connection():
 def closee(cnx):
     if cnx:
         cnx.close()
-cur = connection().cursor()
-cur.execute("SELECT CURDATE()")
-row = cur.fetchone()
-i=0
-cur.execute("SELECT GREATEST(id) AS max_value FROM car;")
-delo1=str(input())
-cur.execute("INSERT INTO car (id, date_, delo, done) VALUES ({}, {}, {}, false);".format(i, row[0], delo1))
-closee(cur)
+
+def sett(delo1):
+    cnx = connection()
+    cur = cnx.cursor()
+
+    cur.execute("SELECT CURDATE()")
+    row = cur.fetchone()[0]
+
+    cur.execute("use mdb;")
+    cur.execute("""
+    SELECT max(id)
+    FROM car;
+    """)
+    i=cur.fetchall()[0][0]
+    if i:
+        i+=1
+    else:
+        i=1
+
+    cur.execute("INSERT INTO car (id, date_, delo, done) VALUES (%s, %s, %s, false);", (i, row, delo1))
+    cnx.commit()
+    closee(cnx)
+
+def donee(id):
+    cnx = connection()
+    cur = cnx.cursor()
+    cur.execute("use mdb;")
+    cur.execute("Update car set done=1 where id = %s", (id, ))
+    cnx.commit()
+def printt():
+    cnx = connection()
+    cur = cnx.cursor()
+    cur.execute("use mdb;")
+    cur.execute("select * from car")
+    p=cur.fetchall()
+    print(*p)
+    closee(cnx)
+def delete():
+    cnx = connection()
+    cur = cnx.cursor()
+    cur.execute("use mdb;")
+    cur.execute("delete from car")
+    cnx.commit()
+    closee(cnx)
+
+print("Че хочешь?")
+print("1 - Добавить дело, 2 - отметить че сделал, 3 - удалить всё нахуй")
+i=int(input())
+if i==1:
+    message=str(input())
+    sett(message)
+    printt()
+elif i==2:
+    print("Давай id")
+    id=int(input())
+    donee(id)
+    printt()
+elif i==3:
+    delete()
+    printt()
+
